67b9739a4f1f680e83d3618db75c11b7b4751f04
CodiDavidFredman <molodeg.dp@gmail.com>
2022-05-18 21:27:17 +0300

    new

--- controler/page/expenses.php
+++ controler/page/expenses.php
@@ -3,7 +3,7 @@ include_once $_SERVER['DOCUMENT_ROOT']."/model/page/expenses.php";
 include_once $_SERVER['DOCUMENT_ROOT']."/view/lung/expenses.php"; 
 
 // $params = array('wallets','category','counterparty','consumable_services','points','project');
-$params = array('wallets','category','counterparty','consumable_services');
+$params = array('wallets','category','counterparty');
 
 $data = array();
 foreach($params as $param){
