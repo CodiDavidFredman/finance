f7da899b21634cf14ee0bcb131b9b15d619c0327
CodiDavidFredman <molodeg.dp@gmail.com>
2022-05-18 21:23:13 +0300

    all

--- controler/page/expenses.php
+++ controler/page/expenses.php
@@ -2,7 +2,9 @@
 include_once $_SERVER['DOCUMENT_ROOT']."/model/page/expenses.php"; 
 include_once $_SERVER['DOCUMENT_ROOT']."/view/lung/expenses.php"; 
 
-$params = array('wallets','category','counterparty','consumable_services','points','project');
+// $params = array('wallets','category','counterparty','consumable_services','points','project');
+$params = array('wallets','category','counterparty','consumable_services');
+
 $data = array();
 foreach($params as $param){
     $data[$param] = qurery($mysql,$param);
